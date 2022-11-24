6dfe19e7dcc3fdced836b980abfe01dfdca05d95
ImanDanl <108823491+ImanDanl@users.noreply.github.com>
2022-11-24 14:50:03 +0800

    Add files via upload

---
+++ padam.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_GET["id"] ) ) {
+    $id = $_GET["id"];
+    
+    $servername = "localhost" ;
+    $username = "root" ;
+    $password = "";
+    $database = "datapelajar" ;
+
+    // Create connection 
+    $connection = new mysqli($servername, $username, $password, $database);
+
+    $sql = "DELETE FROM info_pelajar WHERE id=$id";
+    $connection->query($sql);
+    
+}
+
+header("location:/megaholdings/index.php");
+exit;
+?>
